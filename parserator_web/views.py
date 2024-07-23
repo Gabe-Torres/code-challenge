d837a2f37c1e9a1ab24ba5bf05c7340ff9c25eaa
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-07-22 21:31:42 -0500

    create: views and index view for address parser

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,9 +16,26 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input_string = request.GET.get('address', '')
+        if not input_string:
+            raise ParseError("No address provided")
+
+        address_components, address_type = self.parse(input_string)
+
+        if address_components is None:
+            raise ParseError("Invalid address format")
+
+        return Response({
+            'input_string': input_string,
+            'address_components': address_components,
+            'address_type': address_type
+        })
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        try:
+            address_components, address_type = usaddress.tag(address)
+        except usaddress.RepeatedLabelError:
+            address_components, address_type = None, "Invalid"
         return address_components, address_type
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,27 @@
-import pytest
-
-
 def test_api_parse_succeeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = client.get(f'/api/parse/?address={address_string}')
+    assert response.status_code == 200
+    data = response.json()
+    assert 'address_components' in data
+    assert 'AddressNumber' in data['address_components']
+    assert 'PlaceName' in data['address_components']
+    assert 'StateName' in data['address_components']
+    assert 'StreetName' in data['address_components']
+    assert data['address_components']['AddressNumber'] == '123'
+    assert data['address_components']['PlaceName'] == 'chicago'
+    assert data['address_components']['StateName'] == 'il'
+    assert data['address_components']['StreetName'] == 'main'
 
 
 def test_api_parse_raises_error(client):
     # TODO: Finish this test. The address_string below will raise a
     # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    response = client.get(f'/api/parse/?address={address_string}')
+    assert response.status_code == 400
+    data = response.json()
+    assert 'detail' in data
+    assert data['detail'] == 'Invalid address format'
